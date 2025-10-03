8e81cffb0d1d6dac82ff833c1b009d6e6e9697cb
Alexey <a03221983@gmail.com>
2025-10-03 20:00:58 +0300

    7777

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 const userinput = document.querySelector('#name-input');
 const username = document.querySelector('#name-output');
 
-userinput.addEventListener('input', event => {
-    username.textContent = userinput.value.trim();
-})
+userinput.addEventListener('input', () => {
+    const name = userinput.value.trim();
+    username.textContent = name === '' ? 'Anonymous' : name;
+});
